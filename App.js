1cdafde44685ddcce7201505d3bdc90278034a4f
Ali Mert <mail@alimert.net>
2021-05-03 16:37:49 +0300

    code

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import useSWR from "swr";
+import { cache } from "swr"
+
+
 
 export default function App() {
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  const { data, error } = useSWR('https://60310457081a010017546b82.mockapi.io/test', fetcher)
+  if (error) return <Text>failed to load</Text>
+  if (!data) return <Text>loading...</Text>
+
+  function logout() {
+    cache.clear()
+    alert("logged out")
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Logout" onPress={logout}></Button>
+      <Text>{JSON.stringify(data)}</Text>
       <StatusBar style="auto" />
     </View>
   );
